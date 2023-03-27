036044902b48169a0ab53fd9441600def6069d21
Maxis <maxi.schuster@icloud.com>
2023-03-27 14:09:00 +0200

    Updated awsConfig after leak
    No keys for you...

--- src/javascripts/awsConfig.js
+++ src/javascripts/awsConfig.js
@@ -1,8 +1,8 @@
 import AWS from "aws-sdk";
 
 AWS.config.update({
-  accessKeyId: "AKIA5XVMW33JXM254SVR",
-  secretAccessKey: "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",
+  accessKeyId: "AKIA5XVMW33J3CQATU6A",
+  secretAccessKey: "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",
   region: "eu-central-1",
 });
 
