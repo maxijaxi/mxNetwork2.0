8808f7f025a5413e0378fd5c8f08d7da8cbfa10d
Maxis <maxi.schuster@icloud.com>
2023-03-30 00:38:33 +0200

    "About" rework

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,136 +1,167 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { DropdownContext } from "../contexts/dropdownContext";
 
 import useTitle from "../hooks/useTitle";
 import useDesc from "../hooks/useDesc";
 import useKeywords from "../hooks/useKeywords";
 
-import SmallBanner from "../components/SmallBanner";
-import Container from "../components/Containers";
+import "../scss/about.scss";
 
-import "../styles/about.css";
+//! MAKE BUTTON LINKS AND PAGES 
+// TODO HTML OLD MXNET
 
 function About() {
-  useTitle("About me");
-  useDesc("This is text");
-  useKeywords("This, is, test"); 
-  return (
-    <div>
-      <SmallBanner url="/images/smallBanner.jpg" />
-      <section>
-        <div className="grid-wrapper">
-          <Container name="About me" id="aboutMe" className="about-container">
-            <h3>Who am I?</h3>
-            <p>
-              Hello, my name is Max, but some people know me as Maxwell or
-              Skable. I'm the creator of the mxNetwork and maxijaxi.net, and as
-              a student, my time is mostly devoted to school. Despite my busy
-              schedule, my passion for technology and programming fuels my free
-              time, which I often spend either playing video games or coding.
-            </p>
-            <br />
-            <h3>My programming journey</h3>
-            <p>
-              I actually started my first project a long time ago, some months
-              before the Covid outbreak. I intended to make my own Discord bot,
-              although this did not last long. Fast forward to about half a year
-              ago, I picked up programming by beginning to learn HTML, CSS, and
-              web development. While it was quite overwhelming at first, I
-              managed to master it after some time, and now I am moving on to
-              some more interesting and modern techniques like React.js (the
-              framework I used for this website).
-            </p>
-            <p>
-              I also picked up my old Discord bot and started from scratch, but
-              this time it is actually a lot more interesting. My guess is that
-              I just lacked the basic knowledge, but now that I have it, I enjoy
-              programming a lot.
-            </p>
-            <br />
-          </Container>
-          <article>
-            <div>
-              <h3>mxNetwork</h3>
-              <p>
-                The name mxNetwork comes from my first homepage, which was
-                initially just a small page to connect all my projects and
-                served as a hub. That is also the origin of both its name and
-                the home page title. After moving on with development, I
-                realized the importance of improving it, not only because it
-                looked horrible on certain screen sizes but also because of its
-                poor performance due to its background gif (although I can't
-                deny that I still like its design).
-              </p>
-              <p>
-                So mxNetwork 2.0 was born, with better looks, performance,
-                features, animations, and much more. I also used React.js to
-                increase productivity, performance and to learn an essential
-                framework in modern web development.
-              </p>
-            </div>
-            <br />
-            <div>
-              <h3>mxCloud</h3>
-              <p>
-                The mxCloud's original name was 21er Cloud, but I changed this
-                fairly quickly (some people probably know why, but I won't tell
-                everyone). So as the name implies, the mxCloud is an exclusive
-                cloud. In this case, exclusive means that it is only available
-                to people who have been personally invited by me.
-              </p>
-              <p>
-                While development is taking longer than expected due to security
-                and capacity problems, it is only a matter of time until it will
-                be finished. Stay tuned for updates!
-              </p>
-            </div>
-            <br />
-            <div>
-              <h3>mxDiscord</h3>
-              <aside>
-                <img src="https://maxijaxi.net/mxdLogo.svg" alt="" />
-              </aside>
-              <p>
-                The mxDiscord is the newest of these three projects. In fact, it
-                is so new that it is still only in early development.
-              </p>
+  useTitle("About");
+  useDesc(
+    "Learn about mxNetwork, a personal hub of projects and a showcase of modern web development. Discover the story behind its creation, the technologies used, and the team behind it."
+  );
+  useKeywords(
+    "mxNetwork, personal hub, projects, modern web development, story, technologies, team"
+  );
 
-              <p>
-                Let me tell you about what the mxDiscord is supposed to become.
-                Its main purpose will be the routing and display of custom-made
-                websites for the Discord servers of my bot. Furthermore, it
-                should hold a dashboard for specific settings and management of
-                these servers.
-              </p>
-              <p>
-                Let me tell you about what the mxDiscord is supposed to become.
-                Its main purpose will be the routing and display of custom-made
-                websites for the Discord servers of my bot. Furthermore, it
-                should hold a dashboard for specific settings and management of
-                these servers.
-              </p>
-            </div>
-          </article>
-          <Container name="My favorite band" className="about-container">
-            <p>
-              Linkin Park is my favorite band. Although they were most active
-              before I was born or when I was very young, I discovered their
-              music fairly early on, and it became a major part of my youth.
-              When I learned about the passing of Chester Bennington, it made me
-              very sad and had a profound impact on me. So, when I found out
-              about the release of their old unreleased tracks, I was overjoyed.
-              One particular song called "Lost" immediately resonated with me,
-              and I fell in love with it.
-            </p>
-            <div className="iframeWrapper">
-              <iframe
-                id="ifm"
-                src="https://www.youtube.com/embed/7NK_JOkuSVY"
-                title="Lost - Linkin Park"
-              />
-            </div>
-          </Container>
+  const { setClick } = useContext(DropdownContext);
+
+  const toggle = () => {
+    setClick(!setClick);
+  };
+  return (
+    <div className="about">
+      <div className="about-head">
+        <h2>
+          This is the
+          <br />
+          <span>
+            m<span className="fast-flicker">x</span>Netw
+            <span className="flicker">o</span>rk
+          </span>
+        </h2>
+      </div>
+      <section>
+        <div className="container">
+          <p>
+            Welcome to mxNetwork! Our story begins with a humble homepage,
+            created by just one person - me! Initially, it was just a small page
+            to connect all my projects, but it quickly became clear that it
+            needed a major upgrade. Not only did it look terrible on certain
+            screen sizes, but its poor performance was also holding it back.
+          </p>
+          <p>
+            That's why I decided to create mxNetwork 2.0 - a sleek,
+            high-performance website with cutting-edge features, animations, and
+            more. I used the power of React.js to boost productivity and learn
+            the essential skills needed for modern web development. And the best
+            part? It's all the work of one person. Join us and experience the
+            power of mxNetwork today!
+          </p>
+          <div style={{ display: "flex", gap: "0.4rem" }}>
+            <Link to="" onClick={toggle}>
+              <button>Old mxNetwork</button>
+            </Link>
+            <Link to="/thisIs/mx/feedback" onClick={toggle}>
+              <button>Feedback</button>
+            </Link>
+          </div>
+        </div>
+        <img src="/images/mxnLogo.svg" alt="mxNetwork Logo" />
+        <img src="/images/meClass.jpeg" alt="me?" className="round-image" />
+        <div className="container">
+          <h3>About me</h3>
+          <p>
+            Welcome to my world of programming and technology! I'm Max, also
+            known as Maxwell or Skable, and I'm the creator of mxNetwork and
+            maxijaxi.net. When I'm not busy studying, I'm either playing video
+            games or coding.
+          </p>
+          <h3>My programming journey</h3>
+          <p>
+            I actually started my first project a long time ago, some months
+            before the Covid outbreak. I intended to make my own Discord bot,
+            although this did not last long. Fast forward to about half a year
+            ago, I picked up programming by beginning to learn HTML, CSS, and
+            web development. While it was quite overwhelming at first, I managed
+            to master it after some time, and now I am moving on to some more
+            interesting and modern techniques like React.js (the framework I
+            used for this website). I also picked up my old Discord bot and
+            started from scratch, but this time it is actually a lot more
+            interesting. My guess is that I just lacked the basic knowledge, but
+            now that I have it, I enjoy programming a lot.
+          </p>
+          <div style={{ display: "flex", gap: "0.4rem" }}>
+            <Link to="/thisIs/mx/profile" onClick={toggle}>
+              <button>My Profile</button>
+            </Link>
+            <a 
+              href="https://maxijaxi.net/"
+              rel="noreferrer"
+              target="_blank"
+              onClick={toggle}
+            >
+              <button>My Portfolio</button>
+            </a>
+            <Link to="/thisIs/mx/services" onClick={toggle}>
+              <button>My Services</button>
+            </Link>
+          </div>
+        </div>
+        <div className="container">
+          <h3>My big Projects</h3>
+          <p>
+            In addition to mxNetwork, I am also working on some other exciting
+            projects. One of them is mxCloud, an exclusive cloud service that is
+            only available to people who have been personally invited by me.
+            While the development process is taking longer than expected due to
+            security and capacity issues, I am committed to delivering a
+            top-quality product. Keep an eye out for updates!
+          </p>
+          <p>
+            Another project I'm working on is mxDiscord. This project is still
+            in its early stages of development, but I'm excited about its
+            potential. The goal of mxDiscord is to provide custom-made websites
+            for Discord servers and a dashboard for managing those servers. If
+            you want to learn more about these projects and my other works,
+            click the button below to visit my Projects page.
+          </p>
+          <div style={{ display: "flex", gap: "0.4rem" }}>
+            <Link to="/projects" onClick={toggle}>
+              <button>Projects</button>
+            </Link>
+            <Link to="/updates" onClick={toggle}>
+              <button>Updates</button>
+            </Link>
+            <a
+              href="https://cloud.maxijaxi.net"
+              rel="noreferrer"
+              target="_blank"
+              onClick={toggle}
+            >
+              <button style={{ backgroundColor: "#4dbec2" }}>mxCloud</button>
+            </a>
+            <a
+              href="https://discord.maxijaxi.net"
+              rel="noreferrer"
+              target="_blank"
+              onClick={toggle}
+            >
+              <button style={{ backgroundColor: "#5865F2" }}>mxDiscord</button>
+            </a>
+          </div>
+        </div>
+        <div className="projects-img">
+          <img
+            src="/images/mxcLogo.svg"
+            alt="mxCloud Logo"
+            style={{ alignSelf: "start", justifySelf: "start" }}
+          />
+          <img
+            src="/images/mxdLogo.svg"
+            alt="mxDiscord Logo"
+            style={{ alignSelf: "end", justifySelf: "end" }}
+          />
         </div>
       </section>
+      <br />
+      <br />
     </div>
   );
 }
