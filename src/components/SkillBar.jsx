f91110cfdb48616c55d475783c839a7581b0ec7f
Maxis <maxi.schuster@icloud.com>
2023-04-03 23:22:41 +0200

    fixed & improved SkillBar

--- src/components/SkillBar.jsx
+++ src/components/SkillBar.jsx
@@ -5,6 +5,8 @@ import "../scss/mxMe/skillBar.scss";
 const SkillBar = (props) => {
   const { name, percentage, color } = props;
   const [width, setWidth] = useState(0);
+  const [animatedPercentage, setAnimatedPercentage] = useState(0);
+  const [animationPlayed, setAnimationPlayed] = useState(false);
   const skillBarRef = useRef(null);
 
   useEffect(() => {
@@ -13,14 +15,32 @@ const SkillBar = (props) => {
       const top = skillBarContainer.getBoundingClientRect().top;
       const bottom = skillBarContainer.getBoundingClientRect().bottom;
 
-      if (top >= 0 && bottom <= window.innerHeight) {
+      if (top >= 0 && bottom <= window.innerHeight && !animationPlayed) {
         setWidth(percentage);
+        animatePercentage();
+        setAnimationPlayed(true);
       }
     };
 
+    const animatePercentage = () => {
+      let start = 0;
+      const duration = 1600; // 1.6s animation duration
+      const step = (timestamp) => {
+        if (!start) start = timestamp;
+        const progress = Math.min((timestamp - start) / duration, 1);
+        setAnimatedPercentage(Math.floor(progress * percentage));
+
+        if (progress < 1) {
+          window.requestAnimationFrame(step);
+        }
+      };
+      window.requestAnimationFrame(step);
+    };
+
+    checkIfInView(); // Check if in view on initial load
     window.addEventListener("scroll", checkIfInView);
     return () => window.removeEventListener("scroll", checkIfInView);
-  }, [percentage]);
+  }, [percentage, animationPlayed]);
 
   const isGradient = color.includes("gradient");
 
@@ -29,11 +49,12 @@ const SkillBar = (props) => {
     background: isGradient ? color : `linear-gradient(${color}, ${color})`,
     transition: "width 1.6s ease",
   };
+
   return (
     <div className="skillBar">
       <div className="skill-header">
         <h4>{name}</h4>
-        <strong>{percentage}%</strong>
+        <strong>{animatedPercentage}%</strong>
       </div>
       <div className="skillBarContainer" ref={skillBarRef}>
         <div className="skillBarFill" style={skillBarStyle} />
